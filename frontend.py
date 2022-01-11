b0e2d178715a4e2cad1fbaa51dad91a0a7804050
norway-yv <94965821+norway-yv@users.noreply.github.com>
2022-01-11 08:51:20 +0100

    Delete frontend.py

--- frontend.py
+++
@@ -1,7 +0,0 @@
-import binaryAdd
-import convert
-
-inputOne = convert.toByte(input("First number = "))
-inputTwo = convert.toByte(input("Second number = "))
-
-print(f"Output: {binaryAdd.byte(inputOne, inputTwo)}")
