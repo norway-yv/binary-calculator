d44ba928770e564ddd3848996d203ac616ed3771
norway-yv <94965821+norway-yv@users.noreply.github.com>
2022-01-11 08:51:14 +0100

    Delete convert.py

--- convert.py
+++
@@ -1,7 +0,0 @@
-def toByte(string):
-    if len(string) != 8:
-        temporaryString = string[::-1]
-        for i in range(8 - len(string)):
-            temporaryString += "0"
-        string = temporaryString[::-1]
-    return string
