ab76fd168f2eee327c90e5cf7c691b0792ddcc1f
norway-yv <94965821+norway-yv@users.noreply.github.com>
2022-01-17 13:44:55 +0100

    Added conversion to 32-bit

--- Full/convert.py
+++ Full/convert.py
@@ -5,3 +5,10 @@ def toByte(string):
             temporaryString += "0"
         string = temporaryString[::-1]
     return string
+
+def to32bit(string):
+    if len(string) != 32:
+        temporaryString= string[::-1]
+        for i in range(8 - len(string)):
+            temporaryString += "0"
+        string = temporaryString[::1]
