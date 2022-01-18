bff8f2479fc6e65573bca1000b3f505192c82e48
norway-yv <yrjarvederhus@gmail.com>
2022-01-18 10:43:00 +0100

    Added shortening of output

--- Full/frontend.py
+++ Full/frontend.py
@@ -4,4 +4,4 @@ import convert
 inputOne = convert.to32bit(input("First number = "))
 inputTwo = convert.to32bit(input("Second number = "))
 
-print(f"Output: {binaryAdd.bit32(inputOne, inputTwo)}")
+print(f"Output: {convert.shorten(binaryAdd.bit32(inputOne, inputTwo))}")
