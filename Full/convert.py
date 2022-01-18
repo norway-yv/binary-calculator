fb4b668d36fb206d0d15e56e56b12c020cd736aa
norway-yv <yrjarvederhus@gmail.com>
2022-01-18 10:40:47 +0100

    Added shorten function and reduced local variables

--- Full/convert.py
+++ Full/convert.py
@@ -1,15 +1,20 @@
 def toByte(string):
     if len(string) != 8:
-        temporaryString = string[::-1]
+        string = string[::-1]
         for i in range(8 - len(string)):
-            temporaryString += "0"
-        string = temporaryString[::-1]
+            string += "0"
+        string = string[::-1]
     return string
 
 def to32bit(string):
     if len(string) != 32:
-        temporaryString = string[::-1]
+        string = string[::-1]
         for i in range(32 - len(string)):
-            temporaryString += "0"
-        string = temporaryString[::-1]
+            string += "0"
+        string = string[::-1]
+    return string
+
+def shorten(string):
+    if string[0] != "1":
+        string = string[string.index("1"):]
     return string
