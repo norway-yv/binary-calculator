6451818c703cc48451aaf3020bd2d33b22db7fa9
Yrjar Vederhus <yrjarvederhus@gmail.com>
2022-01-19 11:31:13 +0100

    Fixed minor bug
    Fixed bug with missing "

--- Full/convert.py
+++ Full/convert.py
@@ -23,6 +23,6 @@ def toBit(number, string):
     if len(string) != number:
         string = string[::-1]
         for i in range(number - len(string)):
-            string += 0
+            string += "0"
         string = string[::-1]
     return string
