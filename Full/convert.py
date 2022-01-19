cc343843a6c4dcfac4bfda71b03c56308a9407b1
Yrjar Vederhus <yrjarvederhus@gmail.com>
2022-01-19 11:46:45 +0100

    Removed unnecessary functions

--- Full/convert.py
+++ Full/convert.py
@@ -1,19 +1,3 @@
-def toByte(string):
-    if len(string) != 8:
-        string = string[::-1]
-        for i in range(8 - len(string)):
-            string += "0"
-        string = string[::-1]
-    return string
-
-def to32bit(string):
-    if len(string) != 32:
-        string = string[::-1]
-        for i in range(32 - len(string)):
-            string += "0"
-        string = string[::-1]
-    return string
-
 def shorten(string):
     if string[0] != "1":
         string = string[string.index("1"):]
