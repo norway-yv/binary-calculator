48eb9a92a5c4c48311f32cf99f10daaf9fa0d163
Yrjar Vederhus <yrjarvederhus@gmail.com>
2022-01-19 11:27:37 +0100

    Added length-independent conversion
    added toBit(number, string) with number being goal length of string

--- Full/convert.py
+++ Full/convert.py
@@ -18,3 +18,11 @@ def shorten(string):
     if string[0] != "1":
         string = string[string.index("1"):]
     return string
+
+def toBit(number, string):
+    if len(string) != number:
+        string = string[::-1]
+        for i in range(number - len(string)):
+            string += 0
+        string = string[::-1]
+    return string
